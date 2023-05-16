d0238f048bd3c21b22ef046641fa4456d01078db
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-16 17:16:55 +0400

    Add ProductDto

---
+++ src/main/java/com/company/productmanagement/dto/ProductDto.java
@@ -0,0 +1,18 @@
+package com.company.productmanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductDto {
+    private Long id;
+    private String name;
+    private double price;
+    private String description;
+    private String image;
+}
