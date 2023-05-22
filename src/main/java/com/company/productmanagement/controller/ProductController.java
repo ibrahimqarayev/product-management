badf05d0a96491f78786f70b6448d95437f05eb9
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-22 23:51:36 +0400

    Add ProductController

--- src/main/java/com/company/productmanagement/controller/ProductController.java
+++ src/main/java/com/company/productmanagement/controller/ProductController.java
@@ -44,6 +44,7 @@ public class ProductController {
 
         return "redirect:/list";
     }
+
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable Long id) {
         productService.deleteById(id);
